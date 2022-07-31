9eb4c88c6f1563774bb418498055d2302f98f8eb
lnacuna <lucasnahuel.acuna@nttdata.com>
2022-07-31 17:19:33 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 25 }}>Kryptocurrencies App</Text>
+    </View>
+  );
+};
+
+export default App;
